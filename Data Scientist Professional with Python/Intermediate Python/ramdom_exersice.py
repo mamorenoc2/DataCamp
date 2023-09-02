7b3831a904982cfc5efd5293b53063f50385a817
Manuela Moreno <49201450+mamorenoc2@users.noreply.github.com>
2023-09-02 12:00:04 -0500

    End random exersice

--- Data Scientist Professional with Python/Intermediate Python/ramdom_exersice.py
+++ Data Scientist Professional with Python/Intermediate Python/ramdom_exersice.py
@@ -5,31 +5,35 @@ import matplotlib.pyplot as plt
 # Set the seed
 np.random.seed(123)
 
-# Initialize random_walk
-random_walk = [0]
+# initialize and populate all_walks
+all_walks = []
 
-# Complete the ___
-for x in range(100) :
-    # Set step: last element in random_walk
-    step = random_walk[-1]
+for i in range(20) :
+    # Initialize random_walk
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
 
-    # Roll the dice
-    dice = np.random.randint(1,7)
+        # Implement clumsiness
+        if np.random.rand() <= 0.005 :
+            step = 0
+            
+        random_walk.append(step)
+    all_walks.append(random_walk)
 
-    # Determine next step
-    if dice <= 2:
-        step = max(0, step - 1)
-    elif dice <= 5:
-        step = step + 1
-    else:
-        step = step + np.random.randint(1,7)
+# Convert all_walks to NumPy array: np_aw
+np_aw = np.array(all_walks)
 
-    # append next_step to random_walk
-    random_walk.append(step)
+# Transpose np_aw: np_aw_t
+np_aw_t = np.transpose(np_aw)
 
-# Plot random_walk
-plt.plot(random_walk)
-
-
-# Show the plot
+# Plot np_aw_t and show
+plt.plot(np_aw_t)
 plt.show()
