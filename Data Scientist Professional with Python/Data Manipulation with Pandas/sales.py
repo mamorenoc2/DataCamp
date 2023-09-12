7355a3272163c61e09a7906072a5b30fbb0a69ea
Manuela Moreno <49201450+mamorenoc2@users.noreply.github.com>
2023-09-12 06:55:23 -0500

    groupby pandas function in python

--- Data Scientist Professional with Python/Data Manipulation with Pandas/sales.py
+++ Data Scientist Professional with Python/Data Manipulation with Pandas/sales.py
@@ -62,7 +62,7 @@ store_types = sales.drop_duplicates(subset=['store', 'type'])
 
 # Drop duplicate store/department combinations
 store_depts = sales.drop_duplicates(subset=['store', 'department'])
-print(store_depts.head())
+#print(store_depts.head())
 
 # Subset the rows where is_holiday is True and drop duplicate dates
 holiday_dates = sales[sales['is_holiday']].drop_duplicates(subset='date')
@@ -71,17 +71,59 @@ holiday_dates = sales[sales['is_holiday']].drop_duplicates(subset='date')
 #print(holiday_dates.head())
 
 # Count the number of stores of each type
-store_counts = store_types.value_counts(subset='type')
+store_counts = store_types['type'].value_counts()
 #print(store_counts)
 
 # Get the proportion of stores of each type
-store_props = store_types.value_counts(subset='type', normalize=True)
+store_props = store_types['type'].value_counts(normalize=True)
 #print(store_props)
 
 # Count the number of each department number and sort
-dept_counts_sorted = store_depts.value_counts(subset='department', sort=True)
-print(dept_counts_sorted)
+dept_counts_sorted = store_depts['department'].value_counts(sort=True)
+#print(dept_counts_sorted)
 
 # Get the proportion of departments of each number and sort
-dept_props_sorted = store_depts.value_counts(subset='department', sort=True, normalize=True)
+dept_props_sorted = store_depts['department'].value_counts(sort=True, normalize=True)
 #print(dept_props_sorted)
+
+
+#DIFICULT WAY
+# Calc total weekly sales
+sales_all = sales['weekly_sales'].agg('sum')
+
+# Subset for type A stores, calc total weekly sales
+sales_A = sales[sales["type"] == "A"]["weekly_sales"].agg('sum')
+
+# Subset for type B stores, calc total weekly sales
+sales_B = sales[sales["type"] == "B"]['weekly_sales'].agg('sum')
+
+# Subset for type C stores, calc total weekly sales
+sales_C = sales[sales["type"] == "C"]['weekly_sales'].agg('sum')
+
+# Get proportion for each type
+sales_propn_by_type = [sales_A, sales_B, sales_C] / sales_all
+#print(sales_propn_by_type)
+
+
+#EASY WAY
+# Group by type; calc total weekly sales
+sales_by_type = sales.groupby("type")["weekly_sales"].sum()
+
+# Get proportion for each type
+sales_propn_by_type = sales_by_type / sum(sales_by_type)
+
+# Group by type and is_holiday; calc total weekly sales
+sales_by_type_is_holiday = sales.groupby(['type', 'is_holiday'])['weekly_sales'].sum()
+#print(sales_by_type_is_holiday.head())
+
+# For each store type, aggregate weekly_sales: get min, max, mean, and median
+sales_stats = sales.groupby('type')['weekly_sales'].agg([np.min, np.max, np.mean, np.median])
+
+# Print sales_stats
+print(sales_stats)
+
+# For each store type, aggregate unemployment and fuel_price_usd_per_l: get min, max, mean, and median
+unemp_fuel_stats = sales.groupby('type')[['unemployment', 'fuel_price_usd_per_l']].agg([np.min, np.max, np.mean, np.median])
+
+# Print unemp_fuel_stats
+print(unemp_fuel_stats)
