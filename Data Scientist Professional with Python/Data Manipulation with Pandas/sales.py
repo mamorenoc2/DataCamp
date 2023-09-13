afb0c7ecdea05d5b86429354b962718c4ac79f22
Manuela Moreno <49201450+mamorenoc2@users.noreply.github.com>
2023-09-13 06:25:55 -0500

    pivot tables in pandas and end project of sales.py

--- Data Scientist Professional with Python/Data Manipulation with Pandas/sales.py
+++ Data Scientist Professional with Python/Data Manipulation with Pandas/sales.py
@@ -120,10 +120,35 @@ sales_by_type_is_holiday = sales.groupby(['type', 'is_holiday'])['weekly_sales']
 sales_stats = sales.groupby('type')['weekly_sales'].agg([np.min, np.max, np.mean, np.median])
 
 # Print sales_stats
-print(sales_stats)
+#print(sales_stats)
 
 # For each store type, aggregate unemployment and fuel_price_usd_per_l: get min, max, mean, and median
 unemp_fuel_stats = sales.groupby('type')[['unemployment', 'fuel_price_usd_per_l']].agg([np.min, np.max, np.mean, np.median])
-
 # Print unemp_fuel_stats
-print(unemp_fuel_stats)
+#print(unemp_fuel_stats)
+
+# PIVOTING METHOD
+# Pivot tables are the standard way of aggregating data in spreadsheets
+# Pivot for mean weekly_sales for each store type
+mean_sales_by_type = sales.pivot_table(values='weekly_sales', index='type', aggfunc=np.mean)
+
+# Print mean_sales_by_type
+#print(mean_sales_by_type)
+
+# Pivot for mean and median weekly_sales for each store type
+mean_med_sales_by_type = sales.pivot_table(values='weekly_sales', index='type', aggfunc=[np.median, np.mean])
+
+# Print mean_med_sales_by_type
+#print(mean_med_sales_by_type)
+
+# Pivot for mean weekly_sales by store type and holiday 
+mean_sales_by_type_holiday = sales.pivot_table(values='weekly_sales', index='type', columns='is_holiday')
+
+# Print mean_sales_by_type_holiday
+#print(mean_sales_by_type_holiday)
+
+# Print mean weekly_sales by department and type; fill missing values with 0
+#print(sales.pivot_table(values='weekly_sales', index='type', columns='department', fill_value=0))
+
+# Print the mean weekly_sales by department and type; fill missing values with 0s; sum all rows and cols
+print(sales.pivot_table(values="weekly_sales", index="department", columns="type", fill_value=0, margins=True))
