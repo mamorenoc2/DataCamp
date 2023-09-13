869e438d46af680d95ad5468c812fb549fd84f94
Manuela Moreno <49201450+mamorenoc2@users.noreply.github.com>
2023-09-13 06:36:02 -0500

    New project temperature.py for practice index

---
+++ Data Scientist Professional with Python/Data Manipulation with Pandas/temperature.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+temperatures = pd.read_csv('Datasets/temperatures.csv')
+
+# Set the index of temperatures to city
+temperatures_ind = temperatures.set_index('city')
+
+# Look at temperatures_ind
+print(temperatures_ind)
+
+# Reset the temperatures_ind index, keeping its contents
+print(temperatures_ind.reset_index())
+
+# Reset the temperatures_ind index, dropping its contents
+print(temperatures_ind.reset_index(drop=True))
