d57cca9c3e02733ba38b0160a7a814ac3d470649
Manuela Moreno <mamorenoc2@gmail.com>
2025-10-20 17:52:48 -0500

    Inner joins in python

--- Data Scientist Professional with Python/Join Data with Pandas/data_merging_basics.py
+++ Data Scientist Professional with Python/Join Data with Pandas/data_merging_basics.py
@@ -4,6 +4,9 @@ import numpy as np
 
 taxi_owners = pd.read_pickle('Datasets/Chicago/taxi_owners.p')
 taxi_veh = pd.read_pickle('Datasets/Chicago/taxi_vehicles.p')
+census = pd.read_pickle('Datasets/Chicago/census.p')
+wards = pd.read_pickle('Datasets/Chicago/ward.p')
+
 
 # Merge the taxi_owners and taxi_veh tables
 taxi_own_veh = taxi_owners.merge(taxi_veh, on='vid', suffixes=('_own','_veh'))
@@ -11,3 +14,13 @@ taxi_own_veh = taxi_owners.merge(taxi_veh, on='vid', suffixes=('_own','_veh'))
 # Print the column names of the taxi_own_veh
 print(taxi_own_veh.columns)
 
+'''
+Tipo de JOIN	Qué devuelve
+INNER JOIN	Solo coincidencias entre ambas tablas
+LEFT JOIN	Todo de la izquierda + coincidencias
+RIGHT JOIN	Todo de la derecha + coincidencias
+FULL JOIN	Todo de ambas, coincidan o no
+CROSS JOIN	Todas las combinaciones posibles
+'''
+
+wards_census = wards.merge(census, on='ward')
