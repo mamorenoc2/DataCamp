a1a860304bb29ca1002cdaac9366279314680ad6
Manuela Moreno <mamorenoc2@gmail.com>
2026-02-24 15:47:02 -0500

    Agregar nueva tabla

--- Associate Data engineer with SQL/Intermediate SQL/codigo_practica.sql
+++ Associate Data engineer with SQL/Intermediate SQL/codigo_practica.sql
@@ -1,13 +1,27 @@
-CREATE TABLE empleados (
-    id INTEGER PRIMARY KEY,
-    nombre TEXT,
-    edad INTEGER,
-    salario REAL
-);
+-- CREATE TABLE empleados (
+--     id INTEGER PRIMARY KEY,
+--     nombre TEXT,
+--     edad INTEGER,
+--     salario REAL
+-- );
 
-INSERT INTO empleados (nombre, edad, salario)
-VALUES 
-('Ana', 25, 2000),
-('Luis', 30, 3000);
+-- INSERT INTO empleados (nombre, edad, salario)
+-- VALUES 
+-- ('Ana', 25, 2000),
+-- ('Luis', 30, 3000);
 
+
+-- CREATE TABLE registros (
+--     id INTEGER PRIMARY KEY,
+--     nombre_producto TEXT,
+--     precio REAL
+-- );
+
+-- INSERT INTO registros (nombre_producto, precio)
+--     VALUES
+--     ('TELEFONO', 3500000),
+--     ('PORTATIL', 4500000);
+
+
+SELECT * FROM registros;
 SELECT * FROM empleados;
